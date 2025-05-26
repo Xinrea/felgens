c01a87639e1e466e073b8736b75f7a271a131551
Xinrea <xinreas@outlook.com>
2025-05-27 01:41:31 +0800

    fix: add wbi sign for danmu api

--- src/http_client.rs
+++ src/http_client.rs
@@ -1,6 +1,9 @@
+use pct_str::{PctString, URIReserved};
+use regex::Regex;
 use reqwest::{header::HeaderMap, Client, Response};
 use serde::Deserialize;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
+use tokio::sync::RwLock;
 use url::Url;
 
 use crate::FelgensResult;
@@ -9,6 +12,7 @@ pub struct HttpClient {
     client: Client,
     base_url: Url,
     header: HeaderMap,
+    wbi_key: RwLock<Option<String>>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -46,6 +50,7 @@ impl HttpClient {
             client: Client::new(),
             base_url: Url::parse("https://api.live.bilibili.com")?,
             header,
+            wbi_key: RwLock::new(None),
         })
     }
 
@@ -64,9 +69,14 @@ impl HttpClient {
     }
 
     pub async fn get_dammu_info(&self, room_id: u64) -> FelgensResult<DanmuInfo> {
+        let parameters = serde_json::json!({
+            "id": room_id,
+            "type": 0,
+        });
+        let sign = self.get_sign(parameters).await?;
         let resp = self
             .get(
-                &format!("xlive/web-room/v1/index/getDanmuInfo?id={}&type=0", room_id),
+                &format!("xlive/web-room/v1/index/getDanmuInfo?{}", sign),
                 None,
             )
             .await?
@@ -89,5 +99,91 @@ impl HttpClient {
             .room_id;
 
         Ok(resp)
+    }
+
+    async fn get_wbi_key(&self) -> FelgensResult<String> {
+        if self.wbi_key.read().await.is_some() {
+            return Ok(self.wbi_key.read().await.clone().unwrap());
+        }
+        let mut wbi_key = self.wbi_key.write().await;
+        let nav_info: serde_json::Value = self
+            .get("https://api.bilibili.com/x/web-interface/nav", None)
+            .await?
+            .json()
+            .await?;
+        let re = Regex::new(r"wbi/(.*).png").unwrap();
+        let img = re
+            .captures(nav_info["data"]["wbi_img"]["img_url"].as_str().unwrap())
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .as_str();
+        let sub = re
+            .captures(nav_info["data"]["wbi_img"]["sub_url"].as_str().unwrap())
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .as_str();
+        let raw_string = format!("{}{}", img, sub);
+        *wbi_key = Some(raw_string.clone());
+        Ok(raw_string)
+    }
+
+    pub async fn get_sign(&self, mut parameters: serde_json::Value) -> FelgensResult<String> {
+        let table = vec![
+            46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49, 33, 9, 42,
+            19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40, 61, 26, 17, 0, 1, 60,
+            51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11, 36, 20, 34, 44, 52,
+        ];
+        let raw_string = self.get_wbi_key().await?;
+        let mut encoded = Vec::new();
+        table.into_iter().for_each(|x| {
+            if x < raw_string.len() {
+                encoded.push(raw_string.as_bytes()[x]);
+            }
+        });
+        // only keep 32 bytes of encoded
+        encoded = encoded[0..32].to_vec();
+        let encoded = String::from_utf8(encoded).unwrap();
+        // Timestamp in seconds
+        let wts = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        parameters
+            .as_object_mut()
+            .unwrap()
+            .insert("wts".to_owned(), serde_json::Value::String(wts.to_string()));
+        // Get all keys from parameters into vec
+        let mut keys = parameters
+            .as_object()
+            .unwrap()
+            .keys()
+            .map(|x| x.to_owned())
+            .collect::<Vec<String>>();
+        // sort keys
+        keys.sort();
+        let mut params = String::new();
+        keys.iter().for_each(|x| {
+            params.push_str(x);
+            params.push('=');
+            // Value filters !'()* characters
+            let value = parameters
+                .get(x)
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .replace(['!', '\'', '(', ')', '*'], "");
+            let value = PctString::encode(value.chars(), URIReserved);
+            params.push_str(value.as_str());
+            // add & if not last
+            if x != keys.last().unwrap() {
+                params.push('&');
+            }
+        });
+        // md5 params+encoded
+        let w_rid = md5::compute(params.to_string() + encoded.as_str());
+        let params = params + format!("&w_rid={:x}", w_rid).as_str();
+        Ok(params)
     }
 }
